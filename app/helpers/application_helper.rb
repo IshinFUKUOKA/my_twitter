b973b74f48a6d5ee051b3bba40465329280cd0d9
IshinFUKUOKA <yoghurt1131@gmail.com>
2014-05-30 10:25:32 +0900

    Addd a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#ページごとに完全なタイトルを返す
+	def full_title(page_title)
+		base_title="My Twitter"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title}|#{page_title}"
+		end
+	end
 end
